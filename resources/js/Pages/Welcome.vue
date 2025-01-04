9b49e6c6df02a6e2014fbabce222948a5fc270f0
LucasOliveira2000 <j-lucas9@hotmail.com>
2025-01-04 13:24:37 -0300

    Welcome
    Poder se registrar

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -46,6 +46,13 @@ const { props } = usePage();
                         >
                             Login
                         </Link>
+                        <Link
+                            :href="route('register')"
+                            class="rounded-md px-3 py-2 bg-green-600 text-white hover:bg-green-700 transition"
+                            >
+                            Registrar
+                        </Link>
+
                     </nav>
                 </div>
             </div>
